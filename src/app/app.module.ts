75c98b72f19eedf13a97aaba7cff801131ee9400
aolje <alejoar_p@hotmail.com>
2022-01-10 17:06:36 -0500

    Seccion 5 terminada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Contador } from './contador/contador.module';
+import { HeroesModule } from './heroes/heroes.module';
+import { DbZModule } from './db-z/db-z.module';
 
 @NgModule({
   declarations: [
     AppComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    Contador,
+    DbZModule
   ],
   providers: [],
   bootstrap: [AppComponent]
